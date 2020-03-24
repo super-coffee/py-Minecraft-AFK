919aa6c150acd89a7ba682b77b1921f53de669b9
jinzhijie <742905606@qq.com>
2020-03-24 15:46:53 +0800

    实现进度条

--- dev.py
+++ dev.py
@@ -1,12 +1,16 @@
 # coding: UTF-8
+# https://github.com/jinzhijie/py-Minecraft-AFK
+import os
+import time
+
 import win32api
 import win32con
 import win32gui
-import os
-import time
+from tqdm import tqdm
+from progress.spinner import Spinner
+
 import keyboard
 import mouse
-from tqdm import tqdm
 
 
 class AFK():
@@ -15,7 +19,7 @@ class AFK():
     operation_list = [
         {'type': 'mouse.left', 'description': '鼠标左键'},
         {'type': 'mouse.right', 'description': '鼠标右键'},
-        {'type': 'mouse.move', 'description': '鼠标移动'},  # TODO
+        {'type': 'mouse.move', 'description': '鼠标移动（测试）'},
         {'type': 'keyboard.input', 'description': '键盘按键'},
         {'type': 'keyboard.enter', 'description': '键盘回车'}
     ]
@@ -62,25 +66,35 @@ class AFK():
         print('>>> 在此窗口按下 ctrl+c 终止运行 <<<')
         print('>>> 在任何地方按下 右alt键 开始操作 <<<')
         while True:
+            time.sleep(0.001)  # 不设延时吃 CPU
             rmenu_status = win32api.GetAsyncKeyState(win32con.VK_RMENU)
             if rmenu_status == has_key_down_0 or rmenu_status == has_key_down_1 or rmenu_status == key_down:
-                for _ in tqdm(range(loop_time), ascii=True):  # ascii=True 可防止多行进度条
-                    callback(*args)
-                input('>>> 按下回车退出 <<<')
-                break
+                if not loop_time == 0:
+                    for _ in tqdm(range(loop_time), ascii=True):  # ascii=True 可防止多行进度条，loop_time 太大直接就炸
+                        callback(*args)
+                    input('>>> 按下回车退出 <<<')
+                    break
+                else:  # 无限循环
+                    spinner = Spinner('正在执行     ')
+                    while True:
+                        spinner.next()
+                        callback(*args)
 
     def ops(self, user_op_type, hwnd):
         op_levels = user_op_type.split('.')  # 把硬件类型和操作分开放入列表
         hardware = op_levels[0]  # 硬件类型  e.g. mouse
         operation = op_levels[1]  # 操作  e.g. right
 
-        loop_time = int(input('循环次数 >>>'))
+        loop_time = int(input('循环次数，0 为无限循环 >>>'))
         if hardware == 'mouse' and not operation == 'move':
-            during_time = float(input('按下持续时间（点击可设为 0） >>>'))
+            during_time = float(input('按下持续时间 >>>'))
             delay_time = float(input('抬起持续时间 >>>'))
             self.do(mouse.press, loop_time, (hwnd, operation, during_time, delay_time))
         elif operation == 'move':  # 无需再次判断是否为鼠标
-            print('全是 bug')
+            print('全是 bug，仅供测试！')
+            distance = int(input('distance >>>'))
+            degree = int(input('degree >>>'))
+            mouse.moving(distance, degree, 5, 4)
         elif hardware == 'keyboard':  # 键盘需要提前判断
             if operation == 'input':
                 keys = input('请输入你的按键 >>>')
--- mouse.py
+++ mouse.py
@@ -1,8 +1,9 @@
+import math
+import time
+
 import win32api
 import win32con
 import win32gui
-import time
-import math
 
 
 def _get_client_center_pos(hwnd):
@@ -23,7 +24,7 @@ def left(hwnd, during_time, delay_time, pos):
     time.sleep(delay_time)
 
 
-def right(hwnd, during_time, delay_time, pos):   
+def right(hwnd, during_time, delay_time, pos):
     win32api.PostMessage(hwnd, win32con.WM_RBUTTONDOWN, 0, pos)
     time.sleep(during_time)
     win32api.PostMessage(hwnd, win32con.WM_RBUTTONUP, 0, pos)
@@ -33,35 +34,41 @@ def right(hwnd, during_time, delay_time, pos):
 def move(hwnd, direction, speed, delay_time):  # TODO
     # win32api.PostMessage(hwnd, win32con.WM_MOUSEMOVE, 0, _get_pos_bin(1366, 768))
     print(bin(_get_pos_bin(20, 20)))
-    print(win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON, _get_pos_bin(20, 20)))
-    print(win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON, _get_pos_bin(50, 20)))
+    print(win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE,
+                               win32con.MK_LBUTTON, _get_pos_bin(20, 20)))
+    print(win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE,
+                               win32con.MK_LBUTTON, _get_pos_bin(50, 20)))
 
 
-def smooth_move(distance, degree, time_usage=5, step=4):
-    mousepos = list(win32api.GetCursorPos())
+def moving(distance, degree, time_usage=5, step=4):
+    mousepos = list(win32api.GetCursorPos())  # 获得mc的基准光标
     deta_X = int(step * math.sin(math.radians(degree)))
     deta_Y = int(step * math.cos(math.radians(degree)))
-    repeat_times = int((distance*2) / step) + 1 if int((distance*2) / step) % 2 else int((distance*2) / step)
+    # 根据用户给定的角度和步进计算出每一步再xy轴上要移动的距离分量
+    repeat_times = int((distance * 2) / step) + 1 if int((distance * 2) / step) % 2 else int((distance * 2) / step)
+    # 根据用户给定的距离和步进计算出达成目标需要的移动次数，并尝试让这个数可被2整除
     time_each_repetition = time_usage / repeat_times
+    # 根据用户给定的时间和移动次数计算出每一次移动的理论用时
     mouse_pos_one = [mousepos[0] + deta_X, mousepos[1] + deta_Y]
     mouse_pos_two = [mousepos[0] - deta_X, mousepos[1] - deta_Y]
-    count, sign = repeat_times, 1
-    correct = 0.001
-    while count:
-        if sign:
+    # 根据基准坐标和移动分量算出两个方向的移动坐标
+    count, sign = repeat_times, 1  # count为计数器， sign为方向标志
+    correct = 0.001  # 时间的矫正，因为移动光标的用时未知请酌情调整
+    while count:  # 但计数器非空时调整鼠标位置
+        if sign:  # 如果标志位为1，往one方向移动
             start = time.time()
             win32api.SetCursorPos(mouse_pos_one)
-            count -= 1
-            if count == int((repeat_times / 4)*3):
+            count -= 1  # 每一次移动计数器减一
+            if count == int((repeat_times / 4) * 3):  # 当移动次数只剩总次数的四分之三时把标志位清零
                 sign = 0
             end = time.time()
-            time_use = end - start
-            time.sleep(time_each_repetition -time_use - correct)
-        else:
+            time_use = end - start  # 计算本次使用的时间
+            time.sleep(time_each_repetition - time_use - correct)  # 休眠一段时间
+        else:  # 如果标志位为0，往two方向移动
             start = time.time()
             win32api.SetCursorPos(mouse_pos_two)
             count -= 1
-            if count == int(repeat_times / 4):
+            if count == int(repeat_times / 4):  # 大同小异
                 sign = 1
             end = time.time()
             time_use = end - start
@@ -78,5 +85,4 @@ def press(hwnd, button, during_time, delay_time):
 
 if __name__ == '__main__':  # DEBUG
     time.sleep(5)
-    # move(197668, 0, 0, 0)
-    smooth_move(100, 700)
+    moving(500, 90, 5, 4)

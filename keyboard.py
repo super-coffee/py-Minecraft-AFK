f82410e6fa305927022a1de2100602e0db93b2a3
CharlieYu <charlieyu4994@outlook.com>
2020-03-25 22:14:42 +0800

    优化流程

--- keyboard.py
+++ keyboard.py
@@ -16,36 +16,20 @@ class KeyOperate:
     keys = None
 
     def __init__(self, hwnd, keys):
-        self.hwnd = hwnd
+        self.hwnd = hwnd  # 获取句柄和按键
         self.keys = keys
 
-    def keydown(self):
-        for key in self.keys:
-            # 得到 wparam
-            VkKeyCode = win32api.VkKeyScan(key)
-            # 得到 lparam
-            lparamDown = _get_lparam(VkKeyCode, False)
-            # PostMessage
-            win32api.PostMessage(self.hwnd, win32con.WM_KEYDOWN, VkKeyCode, lparamDown)
-            time.sleep(0.0001)
-
-    def keyup(self):
-        for key in self.keys:
-        # 得到 wparam
-            VkKeyCode = win32api.VkKeyScan(key)
-            # 得到 lparam
-            lparamUp = _get_lparam(VkKeyCode, True)
-            # PostMessage
-            win32api.PostMessage(self.hwnd, win32con.WM_KEYDOWN, VkKeyCode, lparamUp)
-            time.sleep(0.0001)
-
-
-
-
-def enter(hwnd):
-    # 得到 wparam
-    wparam = win32con.VK_RETURN
-    # 得到 lparam
-    lparam = _get_lparam(wparam, False)
-    # PostMessage
-    win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, wparam, lparam)
+    def key(self, is_enter=False, is_up=False):  # 传入两个参数，是否为回车和是否为按下按键
+        wparam = win32con.WM_KEYUP if is_up else win32con.WM_KEYDOWN
+        # 根据输入得出wparam
+        if is_enter:  # 判断是否为回车
+            VkKeyCode = win32con.VK_RETURN  # 赋值回车的虚拟键值
+            lparam = _get_lparam(VkKeyCode, is_up)  # 计算lparam
+            win32api.PostMessage(self.hwnd, wparam, VkKeyCode, lparam)
+            time.sleep(0.001)
+        else:
+            for key in self.keys:  # 遍历keys，操作所有定义的按键
+                VkKeyCode = win32api.VkKeyScan(key)  # 获得按键的虚拟键值
+                lparam = _get_lparam(VkKeyCode, is_up)  # 接下来大同小异
+                win32api.PostMessage(self.hwnd, wparam, VkKeyCode, lparam)
+                time.sleep(0.001)

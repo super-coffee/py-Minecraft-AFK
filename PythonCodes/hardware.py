50a3ce7755791a719eae291ee1d1aee57929d52d
CharlieYu <charlieyu4994@outlook.com>
2020-04-24 15:12:37 +0800

    瞎整活

--- PythonCodes/hardware.py
+++ PythonCodes/hardware.py
@@ -3,12 +3,18 @@ import os
 import win32api, win32con
 from universal_function import get_lparam
 from universal_function import _do_postmessage
+from universal_function import do_postmessage
 from universal_function import default_callback
 from universal_function import nonblocking_delay
+from universal_function import get_client_center_pos
 
 
-class Keyboard:
-	# --------------------------------定义一些变量------------------------------------- #
+
+class hardware:
+	"""
+	这个是鼠标和键盘的基类
+	"""
+
 	hwnd = None
 	keys = None
 	delay_time = None
@@ -20,6 +26,18 @@ class Keyboard:
 		self.keys = keys
 		self.delay_time = delay_time
 		self.during_time = during_time
+
+
+class Keyboard(hardware):
+	"""
+	这个是键盘用的类
+
+	Init方法的参数:\n
+	1.传窗口句柄\n
+	2.按键或者要发送的字符串\n
+	3.延迟时间或者按键抬起时间\n
+	4.按键按下时间
+	"""
 
 	# ---------------------------------按键操作--------------------------------------- #
 	def operate(self, callback=default_callback, args=None):  # 传入两个参数，是否为回车和是否为按下按键
@@ -45,6 +63,43 @@ class Keyboard:
 			end = time.time()
 			if end - start >= self.delay_time:
 				break
+
+
+class Mouse(hardware):
+	"""
+	这个是键盘用的类
+
+	Init方法的参数:\n
+	1.传窗口句柄\n
+	2.按键或者完成鼠标一点的用时\n
+	3.按键抬起时间或者鼠标移动角度\n
+	4.按键按下时间或者鼠标移动距离
+	"""
+
+	# ---------------------------------按键操作--------------------------------------- #
+	def press(self, callback=default_callback, args=None):
+		pos = get_client_center_pos(self.hwnd)
+		if self.keys == 0:
+			mouse_key = [win32con.WM_LBUTTONDOWN, win32con.WM_LBUTTONUP]
+		elif self.keys == 1:
+			mouse_key = [win32con.WM_RBUTTONDOWN, win32con.WM_RBUTTONUP]
+		else:
+			mouse_key = [win32con.WM_MBUTTONDOWN, win32con.WM_MBUTTONUP]
+		do_postmessage(self.hwnd, self.during_time, self.delay_time, mouse_key, pos, callback, args)
+
+	# ---------------------------------移动操作-------------------------------------- #
+	def move(self, callback=default_callback, args=None):
+		args.append(0), args.append(0), args.append(True)
+		os.popen(f"..\\plugins\\MOUSEMOVE.exe {self.during_time} {self.delay_time} {self.keys} 5")
+		start = time.time()
+		while True:
+			callback(args)
+			time.sleep(0.01)
+			end = time.time()
+			if end - start >= 5:
+				break
+
+
 
 
 if __name__ == "__main__":
